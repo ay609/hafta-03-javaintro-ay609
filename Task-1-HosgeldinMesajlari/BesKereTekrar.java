483db5257c97df5f09818d53f021fe919a750ab0
aycha alebrahım <250541614@firat.edu.tr>
2025-10-23 23:19:40 +0300

    Update BesKereTekrar.java
    beskere.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,9 @@
+public class BesKere {
+    public static void main(String[] args) {
+        System.out.println("Java’ya Hoş Geldiniz");
+        System.out.println("Java’ya Hoş Geldiniz");
+        System.out.println("Java’ya Hoş Geldiniz");
+        System.out.println("Java’ya Hoş Geldiniz");
+        System.out.println("Java’ya Hoş Geldiniz");
+    }
+}
