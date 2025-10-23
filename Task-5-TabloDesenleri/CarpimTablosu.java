fca1a24f508ebcb40b06a8606ec4b65449d14fa9
aycha alebrahım <250541614@firat.edu.tr>
2025-10-23 23:44:23 +0300

    Update CarpimTablosu.java
    Update CarpimTablosu.java

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,8 @@
+public class CarpimTablosu {
+    public static void main(String[] args) {
+        System.out.println("2'nin Çarpım Tablosu:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("2 x " + i + " = " + (2 * i));
+        }
+    }
+}
