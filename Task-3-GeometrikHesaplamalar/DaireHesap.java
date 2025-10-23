02fbbc1607db4750366054d9c2819c32270e64cf
aycha alebrahım <250541614@firat.edu.tr>
2025-10-23 23:33:28 +0300

    Update DaireHesap.java
    Update DaireHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,13 @@
+public class DaireHesap {
+    public static void main(String[] args) {
+        double r = 5.5;
+        double pi = Math.PI;
+
+        double alan = pi * r * r;
+        double cevre = 2 * pi * r;
+
+        System.out.println("Yarıçap : " + r);
+        System.out.println("Alan : " + alan);
+        System.out.println("Çevre : " + cevre);
+    }
+}
