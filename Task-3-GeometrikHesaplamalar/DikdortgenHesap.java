05efc186adba11245c28f09fad6057d627140bbe
aycha alebrahım <250541614@firat.edu.tr>
2025-10-23 23:34:55 +0300

    Update DikdortgenHesap.java
    Update DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,11 @@
+public class Dikdortgen {
+    public static void main(String[] args) {
+        double en = 4.5, boy = 7.9;
+
+        double alan = en * boy;
+        double cevre = 2 * (en + boy);
+
+        System.out.println("Dikdörtgen Alanı : " + alan);
+        System.out.println("Dikdörtgen Çevresi : " + cevre);
+    }
+}
