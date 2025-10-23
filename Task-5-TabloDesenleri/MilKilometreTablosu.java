a61c8907f7c7e349bf0f760acc05601a8db48c42
aycha alebrahım <250541614@firat.edu.tr>
2025-10-23 23:46:30 +0300

    Update MilKilometreTablosu.java
    Update MilKilometreTablosu.java

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,13 @@
+public class Donusum {
+    public static void main(String[] args) {
+        double oran = 1.609;
+
+        System.out.println("Mil\tKilometre");
+        System.out.println("----------------");
+        System.out.println("1\t" + (1 * oran));
+        System.out.println("5\t" + (5 * oran));
+        System.out.println("10\t" + (10 * oran));
+        System.out.println("20\t" + (20 * oran));
+        System.out.println("50\t" + (50 * oran));
+    }
+}
