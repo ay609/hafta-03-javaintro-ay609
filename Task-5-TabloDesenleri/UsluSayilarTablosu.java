28ee4c0e6e01d3b1dca4fb2c4b202258e13551f6
aycha alebrahım <250541614@firat.edu.tr>
2025-10-23 23:48:48 +0300

    Update UsluSayilarTablosu.java
    Update UsluSayilarTablosu.java

---
+++ Task-5-TabloDesenleri/UsluSayilarTablosu.java
@@ -0,0 +1,11 @@
+public class UsluSayilar {
+    public static void main(String[] args) {
+        System.out.println("a\t a^2\t a^3");
+        System.out.println("-----------------");
+        System.out.println("1\t " + (1*1) + "\t " + (1*1*1));
+        System.out.println("2\t " + (2*2) + "\t " + (2*2*2));
+        System.out.println("3\t " + (3*3) + "\t " + (3*3*3));
+        System.out.println("4\t " + (4*4) + "\t " + (4*4*4));
+        System.out.println("5\t " + (5*5) + "\t " + (5*5*5));
+    }
+}
