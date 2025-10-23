b2ad6ee66e820a70b1e75821f71a78f9bae2df6e
aycha alebrahım <250541614@firat.edu.tr>
2025-10-23 23:17:54 +0300

    Update UcMesaj.java
    ucmesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,7 @@
+public class UcMesaj {
+    public static void main(String[] args) {
+        System.out.println("Java’ya Hoş Geldiniz");
+        System.out.println("Bilgisayar Bilimine Hoş Geldiniz");
+        System.out.println("Programlama Eğlencelidir");
+    }
+}
