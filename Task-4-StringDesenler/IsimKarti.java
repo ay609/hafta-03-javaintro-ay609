1fdf596ab9438eed4d428d53a48b5b7757343539
aycha alebrahım <250541614@firat.edu.tr>
2025-10-23 23:41:36 +0300

    Update IsimKarti.java
    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,17 @@
+public class IsimKarti {
+    public static void main(String[] args) {
+        String ad = "Ayça";
+        String soyad = "Alebrahim";
+        String ogrNo = "230301001";
+        String bolum = "Yazılım Mühendisliği";
+
+        System.out.println("====================");
+        System.out.println("     ÖĞRENCİ BİLGİLERİ");
+        System.out.println("====================");
+        System.out.println("Ad      : " + ad);
+        System.out.println("Soyad   : " + soyad);
+        System.out.println("No      : " + ogrNo);
+        System.out.println("Bölüm   : " + bolum);
+        System.out.println("====================");
+    }
+}
